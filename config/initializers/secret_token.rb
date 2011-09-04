101621f1d5ea62c8bafa41b9451f5188e2d5accf
Christian Frugard <frugardc@gmail.com>
2011-09-04 11:22:18 -0400

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bigfish::Application.config.secret_token = 'dedd23ba847fbab146fbaf68a325350204536a13364d544498174013de4683eb5078388e1ca4047b5f9399c626ec3933fbb14677d951ca3eb8bee376572107a8'
